b2424a32596b87e9310fbab23813b4b56484e5d7
Gilson Bezerra <gilson.bezerra@live.com>
2025-11-16 15:32:15 -0300

    Develop (#11)
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * Fix gradlew permission
    
    * Fix gradlew permission
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "br.com.lithiumcode.analyticssdk"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0"
+        versionCode
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
