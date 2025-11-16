a636fdeb67948d8da645cc186d6bbe79a2f3e72a
Gilson Bezerra <gilson.bezerra@live.com>
2025-11-16 14:00:12 -0300

    Master (#6)
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline

--- analytics-sdk/build.gradle.kts
+++ analytics-sdk/build.gradle.kts
@@ -7,9 +7,7 @@ plugins {
 
 android {
     namespace = "br.com.lithiumcode.analytics_sdk"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
@@ -26,25 +24,18 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
 }
 
@@ -67,8 +58,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.play.services.measurement.api)
     implementation(libs.androidx.compose.runtime)
+    implementation(libs.play.services.measurement.api)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
