4c168f30816ae9e141e1b7a6efddf175813bb912
Gilson Bezerra <gilson.bezerra@live.com>
2025-11-16 02:31:27 -0300

    feat: cria a branch develop

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,8 +11,7 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("org.jetbrains.kotlin.android") version "2.0.0"
-        id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
     }
 }
 dependencyResolutionManagement {
