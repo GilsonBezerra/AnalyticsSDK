8c6fcb30698a9ddb0317f9f30025b6a1fb9592c2
Gilson Bezerra <gilson.bezerra@live.com>
2025-11-17 01:31:56 -0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,11 @@ android {
     buildFeatures {
         compose = true
     }
+    configurations.all {
+        resolutionStrategy {
+            force("com.google.android.gms:play-services-measurement:23.0.0")
+        }
+    }
 }
 
 dependencies {
@@ -52,6 +57,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
 
     // LIBS
     implementation(project(":analytics-sdk"))
