8cd3079b524f98ff0e2ea0edf021b9f336607917
Gilson Bezerra <gilson.bezerra@live.com>
2025-11-16 23:29:22 -0300

    Develop (#28)
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * Fix gradlew permission
    
    * Fix gradlew permission
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * refatora gradle
    
    * teste
    
    * pipeline
    
    * version
    
    * version
    
    * version update
    
    * resolve
    
    * Projeto teste itau
    
    * pipeline
    
    * version update
    
    * muda de master para main
    
    * muda de master para main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode
-        versionName = "1.0.8"
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
