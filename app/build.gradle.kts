d7e2bb211262ad4e536525c2ec1547f53e22c370
Gilson Bezerra <gilson.bezerra@live.com>
2025-11-17 01:33:03 -0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode
-        versionName = "1.0.11"
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
