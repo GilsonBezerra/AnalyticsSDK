4778cec17a52d626484f76f6a09eb89cea7d23f3
Gilson Bezerra <gilson.bezerra@live.com>
2025-11-18 21:21:43 -0300

    Develop (#34)
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * refactor: refaz estrutura da pipeline
    
    * Fix gradlew permission
    
    * Fix gradlew permission
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * Make gradlew executable
    
    * refatora gradle
    
    * teste
    
    * pipeline
    
    * version
    
    * version
    
    * version update
    
    * resolve
    
    * Projeto teste itau
    
    * pipeline
    
    * version update
    
    * muda de master para main
    
    * muda de master para main
    
    * muda de master para main
    
    * muda de master para main
    
    * fix: corrige dependencias do gradle
    
    * Projeto teste itau
    
    * fix
    
    * fix
    
    * version update
    
    * refactor: altera função de trackEvent

--- analytics-sdk/src/main/java/br/com/lithiumcode/analytics_sdk/AnalyticsTracker.kt
+++ analytics-sdk/src/main/java/br/com/lithiumcode/analytics_sdk/AnalyticsTracker.kt
@@ -1,20 +1,25 @@
 package br.com.lithiumcode.analytics_sdk
 
 import android.content.Context
+import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.logEvent
+
 object AnalyticsTracker {
     private var firebaseAnalytics: FirebaseAnalytics? = null
 
     fun initialize(context: Context) {
         firebaseAnalytics = FirebaseAnalytics.getInstance(context)
     }
-    fun trackEvent(name: String, params: Map<String, String>? = null) {
-        firebaseAnalytics?.logEvent(name) {
-            params?.forEach { (key, value) ->
-                param(key, value)
-            }
-        }
-    }
 
+    fun trackEvent(screen: String, event: String, Extraparams: Map<String, String>? = null) {
+        val bundle = Bundle()
+        bundle.putString("screen", screen)
+        bundle.putString("event", event)
+        Extraparams?.forEach { (key, value) ->
+            bundle.putString(key, value)
+        }
+        firebaseAnalytics?.logEvent(event, bundle)
+    }
 }
+
